e1916da9dcd926d911b0e699e3144fb4aa8823de
Mitka Panarin <mitka_j@yahoo.com>
2023-02-24 20:13:29 +0100

    ,

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Cards = ({label, image, calories, source}) => {
+const Cards = ({ label, image, calories, source, id }) => {
   return (
     <div className="card col-3">
-  <img src={image} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{label}</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <Link to={`/Recipe/iodushfcvdhchb`} className="btn btn-primary">View recepie</Link>
-    <p>Calories: {calories}</p>
-    <p>Source: {source}</p>
-  </div>
-</div>
-  )
-}
+      <img src={image} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{label}</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <Link to={`/Recipe/${id}`} className="btn btn-primary">
+          View recepie
+        </Link>
+        <p>Calories: {calories}</p>
+        <p>Source: {source}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Cards
+export default Cards;
